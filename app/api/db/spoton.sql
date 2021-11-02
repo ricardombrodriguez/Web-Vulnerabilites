d0328b8744c45aa40966d40734b12454c59f2788
joaoreis16 <joaoreis16@ua.pt>
2021-11-02 22:55:56 +0000

    docker domne

--- app/api/db/spoton.sql
+++ app/api/db/spoton.sql
@@ -1,28 +1,28 @@
-create database spoton
-go
+create database if not exists spoton;
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT SELECT, INSERT, CREATE, DROP ON spoton.* TO 'admin'@'localhost';
+use spoton;
+
 
 create table users (
-	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	nome VARCHAR(100) NOT NULL,
-	email VARCHAR(150) NOT NULL,
-	pass VARCHAR(100) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    pass VARCHAR(100) NOT NULL
 );
-go
 
 create table trips (
-	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	nome VARCHAR(100) NOT NULL,
-	preco FLOAT not null,
-	descricao VARCHAR(300),
-	lugar  VARCHAR(100) not null,
-	avaliacao int check (avaliacao >= 0 and avaliacao < 6)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DOUBLE not null,
+    descricao VARCHAR(300),
+    lugar  VARCHAR(100) not null,
+    avaliacao int check (avaliacao >= 0 and avaliacao < 6)
 );
-go
 
 create table comment (
-	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	autor int not null,
-	texto VARCHAR(300),
-
-	FOREIGN KEY (autor) references users(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    autor int not null,
+    texto VARCHAR(300),
+    FOREIGN KEY (autor) references users(id)
 );
