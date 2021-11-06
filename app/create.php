c697625a9ad22053152c399d14872355fe37ebc9
dxogo <diogophc@ua.pt>
2021-11-06 02:38:39 +0000

    Create page done

--- app/create.php
+++ app/create.php
@@ -22,7 +22,7 @@
 <!--===============================================================================================-->
 </head>
 <body>
-	
+<?php include "connection.php" ?>
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('img/banner/travel_HD.jpg'); background-position: center;  background-repeat: no-repeat; background-size: cover;">
 			<div class="wrap-login100">
@@ -30,13 +30,13 @@
 					<img src="img/logo.png" style="margin-top: 100px" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form" action="connection.php" method="post" >
+				<form class="login100-form validate-form" method="POST" >
 					<span class="login100-form-title">
 						Create Account
 					</span>
 
                     <div class="wrap-input100 validate-input" data-validate = "Valid username is required: usermame">
-						<input class="input100" type="text" name="_name" placeholder="Name">
+						<input class="input100" type="text" id="name" name="name" placeholder="Name">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-user" aria-hidden="true"></i>
@@ -44,7 +44,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
+						<input class="input100" type="text" id="email" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -52,7 +52,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" id="pass" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -61,14 +61,34 @@
 					
 					<div class="container-login100-form-btn">
 						<a href="./create.php">
-							<button type="button" class="login100-form-btn" style="width:290px">
+							<button type="submit" class="login100-form-btn" style="width:290px" name="submit_btn">
 								Create
 							</button>
 
 					</div>
 
-					<?php 
-					
+					<?php
+						if (isset($_POST['submit_btn'])){
+							
+							if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['pass'])){
+								echo "here";
+								$name = $_POST['name'];
+								$email = $_POST['email'];
+								$pass = $_POST['pass'];
+
+								// Use prepared statements to mitigate SQL injection attacks.
+								$sql = "INSERT INTO `users` (`nome`, `email`, `pass`) VALUES ('$name', '$email.', '$pass')";
+								$result = mysqli_query($conn, $sql);	
+
+								if ($result) {	// se faz o insert com sucesso na base de dados retorna true
+									echo "<script> location.replace('home.php'); </script>";
+								}
+								mysqli_close($conn);
+
+							} else {
+								echo "<div class=\"container-login100-form-btn\" ><p style=\" color: red\">Erro! Verifica q todos os campos estão preenchidos</p> </div>";
+							}
+						}
 					?>
 
 					<div class="text-center p-t-136">
@@ -83,59 +103,6 @@
 	</div>
 
 	<!-- ------------------------------------------------------------- -->
-
-	<?php
-	
-	if (isset($_POST['_name']) && isset($_POST['email']) && isset($_POST['pass'])){
-
-		// $_name=preg_replace('/[^a-zA-Z0-9\ ]/','',$_POST['_name']);
-		// $email=preg_replace('/[^a-zA-Z0-9\ ]/','',$_POST['email']);
-		// $pass=preg_replace('/[^a-zA-Z0-9\ ]/','',$_POST['pass']);
-
-		// /* Escape your input: use htmlspecialchars to avoid most obvious XSS attacks.
-		// * Note: Your application may still be vulnerable to XSS if you use variables in an attribute without proper quoting.
-		// */
-
-		// $_name=htmlspecialchars($_name);
-		// $email=htmlspecialchars($email);
-		// $pass=htmlspecialchars($pass);
-
-		$_name = $_POST['_name'];
-		$email = $_POST['email'];
-		$pass = $_POST['pass'];
-
-	}
-	// Define MySQL connection and credentials
-	$servername = "spoton";
-	$username = "admin";
-	$password = "admin";
-	$databasename = "spoton"; 
-
-	try {
-		// Establish connection to database
-		$conn = new mysqli($servername, $username, $password, $databasename);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		echo "Connected successfully";
-
-		// Use prepared statements to mitigate SQL injection attacks.
-		$sql = "INSERT INTO users (_name, email, pass) VALUES ('".$_name."', '".$email."', '".$pass."')";
-
-		if (mysqli_query($conn, $sql)) {
-			echo "New user added successfully";
-		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-	} catch (mysqli_error $e) {
-		echo 'Error: ' . $e->getMessage() . " file: " . $e->getFile() . " line: " . $e->getLine();
-		exit;
-	}
-
-	mysqli_close($conn);
-?>
 	
 <!--===============================================================================================-->	
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
